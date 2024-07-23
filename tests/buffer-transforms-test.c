1850f4620c446de7e1ceabbcf136703ce9fd583d
Daniel Stone <daniels@collabora.com>
2024-07-23 17:47:15 +0100

    tests: Ameliorate buffer-transforms image loading
    Rather than loading PNGs a load of times, just do it three times, and
    reuse the same underlying SHM storage.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>

--- tests/buffer-transforms-test.c
+++ tests/buffer-transforms-test.c
@@ -29,6 +29,7 @@
 #include <string.h>
 #include <sys/mman.h>
 
+#include "shared/xalloc.h"
 #include "weston-test-client-helper.h"
 #include "weston-test-fixture-compositor.h"
 
@@ -86,6 +87,40 @@ fixture_setup(struct weston_test_harness *harness, const struct setup_args *arg)
 }
 DECLARE_FIXTURE_SETUP_WITH_ARG(fixture_setup, my_setup_args, meta);
 
+#define MAX_BUFFER_SCALE 3
+
+struct global_test_data {
+	struct buffer *buf[MAX_BUFFER_SCALE];
+};
+
+static void *
+global_test_data_init(struct weston_test_harness *harness)
+{
+	struct global_test_data *global_data = xzalloc(sizeof(*global_data));
+	int i;
+
+	for (i = 0; i < MAX_BUFFER_SCALE; i++) {
+		global_data->buf[i] =
+			buffer_from_image_file("basic-test-card", i + 1);
+	}
+
+	return global_data;
+}
+
+static void
+global_test_data_teardown(struct weston_test_harness *harness, void *data_)
+{
+	struct global_test_data *global_data = data_;
+	int i;
+
+	for (i = 0; i < MAX_BUFFER_SCALE; i++)
+		buffer_destroy(global_data->buf[i]);
+
+	free(global_data);
+}
+
+DECLARE_FIXTURE_INIT(global_test_data_init, global_test_data_teardown);
+
 struct buffer_args {
 	int scale;
 	enum wl_output_transform transform;
@@ -113,6 +148,8 @@ TEST_P(buffer_transform, my_buffer_args)
 {
 	const struct buffer_args *bargs = data;
 	const struct setup_args *oargs;
+	struct global_test_data *global_data = _wet_suite_data->user_data;
+	struct buffer *buf = global_data->buf[bargs->scale - 1];
 	struct client *client;
 	bool match;
 	char *refname;
@@ -136,9 +173,8 @@ TEST_P(buffer_transform, my_buffer_args)
 	client->surface = create_test_surface(client);
 	client->surface->width = 10000; /* used only for damage */
 	client->surface->height = 10000;
-	client->surface->buffer = buffer_from_image_file("basic-test-card",
-							 bargs->scale);
-	ensure_wl_buffer(client, client->surface->buffer);
+	client->surface->buffer = buf;
+	ensure_wl_buffer(client, buf);
 	wl_surface_set_buffer_scale(client->surface->wl_surface, bargs->scale);
 	wl_surface_set_buffer_transform(client->surface->wl_surface,
 					bargs->transform);
@@ -147,6 +183,10 @@ TEST_P(buffer_transform, my_buffer_args)
 	match = verify_screen_content(client, refname, 0, NULL, 0, NULL);
 	assert(match);
 
+	wl_buffer_destroy(buf->proxy);
+	buf->proxy = NULL;
+
+	client->surface->buffer = NULL;
 	client_destroy(client);
 	free(refname);
 }
